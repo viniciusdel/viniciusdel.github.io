aedd45351e9e3583a2654a42bd08858ad53c89ff
Seixas <vseixas4857@floridapoly.edu>
2020-09-23 16:22:32 -0400

    branchtest

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
 
+
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
